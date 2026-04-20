22d8dffca1f6a289574c42a16106fc2f0b151e74
seasons@traveality.com <seasons@traveality.com>
2026-04-20 14:04:33 +0100

    feature: added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx()]
+  integrations: [mdx()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
