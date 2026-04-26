00f9aba3159360c41d47f106fb73b32bffdb9c36
seasons@traveality.com <seasons@traveality.com>
2026-04-26 19:12:44 +0100

    udpate: UI experiment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
   adapter: vercel({
     webAnalytics: {
       enabled: true, // set to false when using @vercel/analytics@1.4.0
